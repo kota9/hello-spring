28ed292ae07e83e1fd1538fc6dcf020698d30eb0
kota9 <kjk916@gmail.com>
2022-04-02 00:06:13 +0900

    20 스프링 통합 테스트

--- src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
+++ src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
@@ -160,7 +160,8 @@ public class JdbcMemberRepository implements MemberRepository {
         }
         try {
             if (conn != null) {
-                conn.close();
+//                conn.close();
+                close(conn);
             }
         } catch (SQLException e) {
             e.printStackTrace();
